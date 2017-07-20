c8727b32afd1931fd26d7c0d20e0c27351e0a3c4
Srujan Routhu <srujan.routhu@gmail.com>
2017-07-20 15:02:33 -0400

    a hacked fade effect

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,5 +1,5 @@
 (function () {
-    var HomeController = function ($scope, $http, $interval, Fullscreen) {
+    var HomeController = function ($scope, $http, $interval, $timeout, Fullscreen) {
 
       $scope.render = false
 
@@ -11,6 +11,7 @@
       }
 
       $scope.image = {}
+      $scope.isOpaque = false;
 
       $http({
           method: 'GET',
@@ -19,6 +20,9 @@
           var i = 0
           $scope.image.source = response[i].url
           $scope.image.author = response[i].author
+          $timeout(function () {
+            $scope.isOpaque = true;
+          }, 4750)
           $interval(function () {
               i++
               if (i === response.length - 1) {
@@ -26,12 +30,18 @@
               }
               $scope.image.source = response[i].url
               $scope.image.author = response[i].author
+              $timeout(function () {
+                  $scope.isOpaque = false;
+              }, 250)
+              $timeout(function () {
+                $scope.isOpaque = true;
+              }, 4750)
           }, 5000)
           $scope.render = true
       })
 
     };
 
-    HomeController.$inject = ['$scope', '$http', '$interval', 'Fullscreen'];
+    HomeController.$inject = ['$scope', '$http', '$interval', '$timeout', 'Fullscreen'];
     angular.module('app.backdrop').controller('HomeController', HomeController);
 }());
